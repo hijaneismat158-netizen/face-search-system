64457b19eccddd837b2b02d42d4c3af95f344cbe
hijaneismat158-netizen <hijaneismat158@gmail.com>
2026-06-06 17:26:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log("SCRIPT LOADED");
 // =====================
 const imageDatabase = [
   "./images/827A0231.jpg",
-  "./images/DSC_0493.jpg"
+  "./images/DSC8395.jpg"
 ];
 
 // =====================
